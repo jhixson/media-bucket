6dbc184b95ecd83f775c8bdb44dafe0de45e5fca
Jesse Hixson <hixsonj@gmail.com>
2021-08-31 21:02:55 -0400

    Updates tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
           800: '#1e1f1f',
           700: '#272828',
           600: '#3e3e3f',
+          200: '#e4e3e3',
         },
+      },
+      fontFamily: {
+        body: '"Roboto", Helvetica, Arial, sans-serif;',
+        display: '"Secular One", Helvetica, Arial, sans-serif',
       },
     },
   },
